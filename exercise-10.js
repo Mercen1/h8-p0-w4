834fee3ede10785ca90d53bad8380fdae30132c4
mercen1 <nurhabib102@gmail.com>
2020-03-12 19:03:09 +0700

    perbaikan

--- exercise-10.js
+++ exercise-10.js
@@ -1,37 +1,36 @@
 function changeMe(arr) {
+    if (arr.length === 0) {
+        console.log('""');
+    }
     var data = {};
-     var depan='';
-     var hasil ={};
+    var depan = '';
+    for (i = 0; i < arr.length; i++) {
+        depan = i + 1. + ' ' + arr[i][0] + ' ' + arr[i][1] + ':';
+        data.firstName = arr[i][0];
+        data.lastName = arr[i][1];
+        data.gender = arr[i][2];
+        data.age = 2020 - arr[i][3];
 
-    for(i=0;i<arr.length; i++){
-        depan = i+1. +' ' + arr[i][0]+' '+arr[i][1] + ':';
-
-        data.firstName =arr[i][0]
-        data.lastName = arr[i][1]
-        data.gender = arr[i][2]
-        data.age = 2020 - arr[i][3]
-
-        if(!arr[i][3]|| data.age < 0){
+        if (!arr[i][3] || data.age < 0) {
             data.age = 'Invalid Birth Year'
         }
-        console.log( depan)
-        console.log( data)
+        console.log(depan)
+        console.log(data)
     }
 
     return null;
+}
 
-  }
-  
-  // TEST CASES
-  changeMe([['Christ', 'Evans', 'Male', 1982], ['Robert', 'Downey', 'Male']]); 
-  // 1. Christ Evans:
-  // { firstName: 'Christ',
-  //   lastName: 'Evans',
-  //   gender: 'Male',
-  //   age: 37 }
-  // 2. Robert Downey:
-  // { firstName: 'Robert',
-  //   lastName: 'Downey',
-  //   gender: 'Male',
-  //   age: 'Invalid Birth Year' }
-  changeMe([]); // ""
+// TEST CASES
+changeMe([['Christ', 'Evans', 'Male', 1982],['Robert', 'Downey', 'Male']]);
+// 1. Christ Evans:
+// { firstName: 'Christ',
+//   lastName: 'Evans',
+//   gender: 'Male',
+//   age: 37 }
+// 2. Robert Downey:
+// { firstName: 'Robert',
+//   lastName: 'Downey',
+//   gender: 'Male',
+//   age: 'Invalid Birth Year' }
+changeMe([]); // ""
