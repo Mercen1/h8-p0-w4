be1e0286c6dd0f223618362df9d9df72d64fbb8b
mercen1 <nurhabib102@gmail.com>
2020-03-12 18:05:06 +0700

    perbaikan sdkit

--- exercise-3.js
+++ exercise-3.js
@@ -1,5 +1,14 @@
 function cariMedian(arr) {
     var median;
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                var tampung = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tampung;
+            }
+        }
+    }
     if (arr.length % 2 === 1) {
         median = arr[(arr.length - 1) / 2];
     } else {
